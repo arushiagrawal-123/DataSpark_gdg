68e377fbe26cc659a5458c6ab3ab2ac1edd37150
arushiagrawal-123 <agrawalarushi123@gmail.com>
2026-01-10 16:39:34 +0530

    Refactor sidebar navigation and main content comment

--- DashboardLayout.jsx
+++ DashboardLayout.jsx
@@ -1,39 +1,23 @@
 import React from "react";
-
-
 import { NavLink, Outlet } from "react-router-dom";
 
 function DashboardLayout() {
   return (
     <div className="dashboard-layout">
-      
       {/* Sidebar */}
       <aside className="sidebar">
         <h2>Admin</h2>
         <ul>
-          <li>
-            <NavLink to="/dashboard" end className="nav-link">
-              Dashboard
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/dashboard/complaints" className="nav-link">
-              Complaints
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/dashboard/hotspots" className="nav-link">
-              Hotspots
-            </NavLink>
-          </li>
+          <li><NavLink to="/dashboard" end className="nav-link">Dashboard</NavLink></li>
+          <li><NavLink to="/dashboard/complaints" className="nav-link">Complaints</NavLink></li>
+          <li><NavLink to="/dashboard/hotspots" className="nav-link">Hotspots</NavLink></li>
         </ul>
       </aside>
 
-      {/* Main content changes here */}
+      {/* Main content */}
       <main className="main-content">
         <Outlet />
       </main>
-
     </div>
   );
 }
