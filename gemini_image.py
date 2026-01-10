f1a3cc91f080a5f173bc016ca973805487c5250d
Ketki Kuthe <ketkikuthe2005@gmail.com>
2026-01-10 15:58:37 +0530

    Change API key to use GEMINI_API_KEY environment variable
    Updated API key retrieval method to use environment variable 'GEMINI_API_KEY'.

--- gemini_image.py
+++ gemini_image.py
@@ -2,7 +2,7 @@ from google import genai
 import os
 
 # Configure client using ENV variable
-client = genai.Client(api_key=os.getenv("AIzaSyBNQZUsUN6hmk7zGoielZ7mpt7eESQ2ni0"))
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
 image_url = "https://vdbetqxhwextlybbmc.supabase.co/storage/v1/object/public/campus-issues/dustbins/dust1.jpg"
 
